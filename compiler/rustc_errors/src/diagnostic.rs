861d109a5f49aeea2dd88ae69eba3f960c5adb4c
Esteban Küber <esteban@kuber.com.ar>
2022-12-31 23:13:36 -0800

    More accurate spans for arg removal suggestion

--- compiler/rustc_errors/src/diagnostic.rs
+++ compiler/rustc_errors/src/diagnostic.rs
@@ -625,7 +625,7 @@ impl Diagnostic {
     pub fn multipart_suggestion_with_style(
         &mut self,
         msg: impl Into<SubdiagnosticMessage>,
-        suggestion: Vec<(Span, String)>,
+        mut suggestion: Vec<(Span, String)>,
         applicability: Applicability,
         style: SuggestionStyle,
     ) -> &mut Self {
@@ -634,6 +634,7 @@ impl Diagnostic {
             !(suggestion.iter().any(|(sp, text)| sp.is_empty() && text.is_empty())),
             "Span must not be empty and have no suggestion"
         );
+        suggestion.sort_by_key(|(span, _)| (span.lo(), span.hi()));
 
         self.push_suggestion(CodeSuggestion {
             substitutions: vec![Substitution {
